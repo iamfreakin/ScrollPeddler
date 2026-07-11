cf8de125df63de0fba9160967bca254618fc6d43
qjaqj <qjaqjaqja1877@gmail.com>
2026-07-11 23:35:20 +0900

    chore: Unreal Engine 프로젝트 기반 구성

---
+++ Source/ScrollPeddler/ScrollPeddler.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ScrollPeddler.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(LogScrollPeddler);
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ScrollPeddler, "ScrollPeddler" );
